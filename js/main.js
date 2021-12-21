9a795988b0ad8a3e7070fcc00e6cd04159d87618
Alan Dias <alan_bmz@hotmail.com>
2021-12-21 20:10:51 -0300

    fist commit Js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+/*
+var nome = "Alan"; 
+*/
+var nome = "Alan";
+var n1 = 5;
+var n2 = 10;
+var frase = "Lingua oficial da Russia é o português";
+//alert(" O " + nome + " tem " + idade + " anos ");
+alert(n1 + n2);
+console.log(nome);
+console.log(idade + idade2);
+//console.log(frase.replace("Russia", "Brasil"));
+console.log(frase.toUpperCase("Russia", "Brasil"));
+//console.log(frase.toLocaleLowerCase("Russia", "Brasil"));
+//alert(frase.replace("Russia", "Brasil"));
