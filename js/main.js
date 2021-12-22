9d49ff30a2af34bc8405482f38b5a886966666bb
Alan Dias <alan_bmz@hotmail.com>
2021-12-22 15:37:35 -0300

    final update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,103 @@
+function but(){
+    document.getElementById("Clicou").innerHTML="<b><i>voce clicou</b></i>";
+
+   // console.log("clicou")
+   // alert("obrigado")
+
+}
+function redirecionamento(){
+  //  window.open("https://www.google.com.br/");
+    window.location.href = "https://www.google.com.br/"
+}
+function mouse(){
+    //document.getelementbyid("mouse").innetHTML = "passou o mouse aqui";
+    alert("voce passou o mouse");
+}
+/*
+function soma (n1, n2){
+    return n1+ n2;
+}
+alert (soma(5,10))
+*/
+
+
+/*
+function setReplace(frase,nome,novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(setReplace("cor azul", "azul", "verde" ));
+*/
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade>=18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;   
+}
+
+var idade = prompt("qual sua idade?");
+console.log(validaIdade(idade));
+*/
+/*
+
+var data = new Date();
+alert(data);
+console.log(data.getDay());
+console.log(data.getHours());
+console.log(data.getMinutes());
+var count;
+for(count=0; count <= 3; count++){
+    alert(count)
+}
+*/
+/*
+var count = 0;
+while (count <=5){
+    console.log(count);
+    alert(count)
+    count = count +1;
+}
+*/
+/*
+var idade = prompt("qual usa idade")
+if (idade >= 18) {
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+
+
+/*
+var lista = ["maça", "pera","laranja"];
+//lista.pop();
+lista.push("uva");
+console.log(lista.length);
+console.log(lista.reverse());
+//console.log(lista.toString(0));
+console.log(lista.join("-"));
+
+fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+console.log(fruta.cor);
+*/
 
 /*
 var nome = "Alan"; 
-*/
 var nome = "Alan";
 var n1 = 5;
 var n2 = 10;
 var frase = "Lingua oficial da Russia é o português";
-//alert(" O " + nome + " tem " + idade + " anos ");
+alert(" O " + nome + " tem " + idade + " anos ");
 alert(n1 + n2);
 console.log(nome);
 console.log(idade + idade2);
-//console.log(frase.replace("Russia", "Brasil"));
+onsole.log(frase.replace("Russia", "Brasil"));
 console.log(frase.toUpperCase("Russia", "Brasil"));
-//console.log(frase.toLocaleLowerCase("Russia", "Brasil"));
-//alert(frase.replace("Russia", "Brasil"));
+console.log(frase.toLocaleLowerCase("Russia", "Brasil"));
+alert(frase.replace("Russia", "Brasil"));
+*/
